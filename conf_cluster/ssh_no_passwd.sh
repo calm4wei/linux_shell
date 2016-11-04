166f107d65fb838cd3bf12374b796b53cde1a8bf
alfer <alfer@datacube152.(none)>
2016-11-04 11:55:49 +0800

    add params

--- conf_cluster/ssh_no_passwd.sh
+++ conf_cluster/ssh_no_passwd.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-for i in `cat /etc/hosts | grep 20 | awk '{print $1}'`; do ssh-copy-id -i ~/.ssh/id_rsa.pub root@$i ;done
+USER=`whoami`
+for i in `cat /etc/hosts | grep datacube15 | awk '{print $1}'`; do ssh-copy-id -i ~/.ssh/id_rsa.pub $USER@$i ;done
--- scp_sth.sh
+++ scp_sth.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
+Usage='''$0 <src path> <deth path> <user>'''
+if [ $# -ne 3 ]; then
+    echo "$Usage"
+    exit
+fi
+
 SRC=$1
 DETH=$2
 USER=$3
 HOSTNAME=`exec /bin/hostname`
 
-for i in `cat /etc/hosts | awk '{print $2}' | grep -v "$HOSTNAME"| grep "datacube"`
+for i in `cat /etc/hosts | awk '{print $2}' | grep -v "$HOSTNAME"| grep "datacube1"`
 do
 	scp -r $SRC	$USER@$i:$DETH
 done

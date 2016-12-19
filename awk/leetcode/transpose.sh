ed3c2b64e0a0fe552e473fef9e07744a7844f39a
alfer <alfer@datacube152.(none)>
2016-12-19 16:14:25 +0800

    leetcode awk

---
+++ awk/leetcode/transpose.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# 194. Transpose File
+
+awk '
+{
+    for (i = 1; i <= NF; i++) {
+	if (NR == 1){
+	    s[i] = $i;
+	} else {
+	    s[i] = s[i] " " $i;
+	}
+    }
+}
+END {
+    for (i = 1; s[i] != ""; i++) {
+        print s[i];
+    }
+}' file.txt
